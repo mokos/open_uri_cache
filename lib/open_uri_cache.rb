c045cab79439b3b7a9d45f94254bf49168bff8a5
mokos <mokos@Hidenobu-no-MacBook-Pro.local>
2018-07-01 12:57:51 +0900

    fix

--- lib/open_uri_cache.rb
+++ lib/open_uri_cache.rb
@@ -20,21 +20,27 @@ module OpenUriCache
     Time.parse(time_str)
   end
 
-  def self.open(uri, *rest, cache_dir: DEFAULT_CACHE_DIRECTORY, expiration:, delete_if_expired: true)
+  def self.open(uri, *rest, cache_dir: DEFAULT_CACHE_DIRECTORY, expiration: nil, after: nil, delete_if_expired: true)
+    if after
+      expiration = Time.now + after
+    end
+
+    unless expiration
+      expiratioon = Time.new(3000, 1, 1)
+    end
 
     FileUtils.mkdir_p(cache_dir)
     Dir.chdir(cache_dir) {
 
       search_files(uri).each do |f|
         if get_expiration(f)>Time.now
-          puts 'use cache: ' + f
+          puts 'use cache'
           return File.open(f, *rest)
         end
 
         File.delete f if delete_if_expired
       end
 
-      puts 'new open'
       s = Kernel.open(uri)
       cache_filename = make_file_name(uri, expiration)
       File.open(cache_filename, 'wb+') {|f|
--- spec/open_uri_cache_spec.rb
+++ spec/open_uri_cache_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe OpenUriCache do
     Dir.mktmpdir {|tmpdir|
       tmpdir = './.open_uri_cache'
       url = 'https://twitter.com'
-      doc = OpenUriCache.open(url, cache_dir: tmpdir, expiration: Time.now+10)
-      doc2 = OpenUriCache.open(url, cache_dir: tmpdir, expiration: Time.now+10)
+      doc = OpenUriCache.open(url, cache_dir: tmpdir, after: 10)
+      doc2 = OpenUriCache.open(url, cache_dir: tmpdir, after: 10)
 
       expect(doc.read.to_s).to eq doc2.read.to_s
     }
@@ -23,14 +23,22 @@ RSpec.describe OpenUriCache do
   it 'different result without chache' do
     Dir.mktmpdir {|tmpdir|
       url = 'https://twitter.com'
-      doc = OpenUriCache.open(url, cache_dir: tmpdir, expiration: Time.now+1)
+      doc = OpenUriCache.open(url, cache_dir: tmpdir, after: 1)
       # キャッシュ期間が1秒間
-      # 2秒間スリープするので次回openはキャッシュが効かない
-      sleep(2)
+      # 3秒間スリープするので次回openはキャッシュが効かない
+      sleep(3)
 
-      doc2 = OpenUriCache.open(url, cache_dir: tmpdir, expiration: Time.now+10)
+      doc2 = OpenUriCache.open(url, cache_dir: tmpdir, after: 10)
 
       expect(doc.read.to_s).not_to eq doc2.read.to_s
     }
+  end
+
+  it 'forever' do
+    Dir.mktmpdir {|tmpdir|
+      url = 'https://twitter.com'
+      doc = OpenUriCache.open(url, cache_dir: tmpdir)
+    }
+
   end
 end
