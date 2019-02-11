6460274dfcb82d312734fbf3063f92158663c149
mokos <mokos308@gmail.com>
2019-02-12 00:19:24 +0900

    change cache file naming (url escape -> directory)

--- lib/open_uri_cache.rb
+++ lib/open_uri_cache.rb
@@ -1,26 +1,59 @@
 require "open_uri_cache/version"
 
 require 'fileutils'
+require 'pathname'
 require 'open-uri'
 require 'cgi'
+require 'json'
 
 module OpenUriCache
+  class Cache
+    def initialize(uri, dir)
+      @uri = uri
+      @dir = Pathname.new(dir).join(uri.gsub(/^https?:\/\//, ''))
+      @content_path = "#{@dir}/content"
+      @info_path = "#{@dir}/info.json"
+
+      delete_expired_cache
+    end
+
+    def delete_expired_cache
+      return unless exist?
+      
+      info = JSON.parse File.open(@info_path, 'r') {|f| f.read }
+      if Time.now > Time.parse(info['expiration'])
+        delete
+      end
+    end
+
+    def delete
+      File.delete @content_path 
+      File.delete @info_path
+      Dir.rmdir @dir rescue nil
+    end
+
+    def exist?
+      File.exist? @content_path and File.exist? @info_path
+    end
+
+    def save(content, info)
+      FileUtils.mkdir_p(@dir)
+      File.open(@content_path, 'wb+') {|f|
+        f.write content
+      }
+      File.open(@info_path, 'w+') {|f|
+        f.puts info.to_json
+      }
+    end
+
+    def open(*args)
+      File.open(@content_path, *args)
+    end
+  end
+
   DEFAULT_CACHE_DIRECTORY = "#{ENV['HOME']}/.open_uri_cache"
 
-  def self.make_file_name(uri, expiration)
-    "#{CGI.escape uri} #{expiration}"
-  end
-
-  def self.search_files(uri)
-    Dir.glob("#{CGI.escape uri} *").sort
-  end
-
-  def self.get_expiration(filename)
-    time_str = filename.split(' ')[1..-1].join(' ')
-    Time.parse(time_str)
-  end
-
-  def self.open(uri, *rest, cache_dir: DEFAULT_CACHE_DIRECTORY, expiration: nil, after: nil, delete_if_expired: true)
+  def self.open(uri, *rest, cache_dir: DEFAULT_CACHE_DIRECTORY, expiration: nil, after: nil)
     if after
       expiration = Time.now + after
     end
@@ -29,35 +62,25 @@ module OpenUriCache
       expiration = Time.new(9999, 1, 1)
     end
 
-    FileUtils.mkdir_p(cache_dir)
-    Dir.chdir(cache_dir) {
-
-      search_files(uri).each do |f|
-        if get_expiration(f)>Time.now
-          return File.open(f, *rest)
-        end
-
-        File.delete f if delete_if_expired
-      end
-
+    cache = Cache.new(uri, cache_dir)
+    if cache.exist?
+      return cache.open(*rest)
+    else
       s = Kernel.open(uri, 'rb')
-      cache_filename = make_file_name(uri, expiration)
       begin
-        File.open(cache_filename, 'wb+') {|f|
-          f.write s.read
-        }
+        cache.save(s.read, { expiration: expiration, meta: s.meta })
       rescue
-        File.delete(cache_filename)
+        cache.delete
       end
 
       s.rewind
       return s
-    }
+    end
   end
 end
 
 if $0==__FILE__
-  puts open('http://google.com').read
-  puts open('http://google.com', expiration: Time.now + 10*60).read
-  puts open('http://google.com', expiration: Time.now + 10*60).read
+  puts OpenUriCache.open2('http://google.com', cache_dir: '.open_uri_cache', after: 1).class
+  puts OpenUriCache.open2('http://google.com/', cache_dir: '.open_uri_cache', after: 1).class
+  exit
 end
